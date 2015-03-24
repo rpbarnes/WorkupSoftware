388d59c173071fd094ac7f68bdcd8870c31d9b32
ryan barnes <rbarnes@chem.ucsb.edu>
2015-03-23 17:46:56 -0700

    Fixed returnIntegrals to handle the T1 power problem if the user runs the first t1 at full power.

--- returnIntegrals.py
+++ returnIntegrals.py
@@ -149,6 +149,15 @@ except:
 expParametersFile = fileName + 'parameters.pkl'
 defaultExpParamsFile = 'parameters.pkl'
 defaultDataParamsFile = 'databaseParameters.pkl'
+temp = load_acqu(dirformat(dirformat(fullPath))+'1',return_s = False)# this pull all of the aquisition data
+cnst = temp.get('CNST')
+t1StartingAttenuation = cnst[24]
+if float(t1FirstAttenFullPower) == float(1.0): # this means we ran the first experiment at full attenuation and we need to handle the power series differently.
+    t1FirstAttenFullPower = True
+else:
+    t1FirstAttenFullPower = False
+
+
 
 # Experiment parameters
 dnpExps = r_[5:27] # default experiment numbers
@@ -157,6 +166,7 @@ integrationWidth = 75
 t1StartingGuess = 2.5 ### This is the best guess for what your T1's are, if your T1 fits don't come out change this guess!!
 ReturnKSigma = True ### This needs to be False because my code is broken
 t1SeparatePhaseCycle = True ### Did you save the phase cycles separately?
+t1StartingAttenuation = 22.0
 thresholdE = 0.3
 thresholdT1 = 0.3
 badT1 = []
@@ -173,7 +183,7 @@ if not expExists:
                     't1SeparatePhaseCycle':t1SeparatePhaseCycle,
                     'thresholdE':thresholdE,
                     'thresholdT1':thresholdT1,
-                    'badT1':badT1
+                    'badT1':badT1,
                     }
     dtb.writeDict(expParametersFile,parameterDict)
 else:
@@ -325,7 +335,11 @@ if dnpexp: # only work up files if DNP experiment
     if not expTimeMin:
         print expTitles
         raise ValueError("\n\nThe experiment numbers are not set appropriately, please scroll through the experiment titles above and set values appropriately")
-    t1Power,fl.figurelist = returnSplitPowers(fullPath,'t1_powers.mat',expTimeMin = expTimes.min(),expTimeMax=expTimeMin.data + expTimeMin.data/2,dnpPowers = True,threshold = parameterDict['thresholdT1'],titleString = 'T1 ',firstFigure = fl.figurelist) # this is a hot hack and needs proper fixing later.
+    # I have the same problem with the dnp powers, if the starting attenuation is full attenuation '31.5' then there is no initial jump and we need to deal with it the same way. Right now I pull from constant 24 in the aquisition parameters. This should now work without having to ask the user.
+    if t1FirstAttenFullPower:
+        t1Power,fl.figurelist = returnSplitPowers(fullPath,'t1_powers.mat',expTimeMin = expTimes.min(),expTimeMax=expTimeMin.data + expTimeMin.data/2,dnpPowers = True,threshold = parameterDict['thresholdT1'],titleString = 'T1 ',firstFigure = fl.figurelist)
+    else:
+        t1Power,fl.figurelist = returnSplitPowers(fullPath,'t1_powers.mat',expTimeMin = expTimes.min(),expTimeMax=expTimeMin.data + expTimeMin.data/2,dnpPowers = False,threshold = parameterDict['thresholdT1'],titleString = 'T1 ',firstFigure = fl.figurelist)
     t1Power = list(t1Power)
     t1Power.append(-99.0) # Add the zero power for experiment 304
     t1Power = array(t1Power)
