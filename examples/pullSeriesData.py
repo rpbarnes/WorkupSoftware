9c52a7d5d280b08259da88c2fd6880477a3347cf
ryan barnes <rbarnes@chem.ucsb.edu>
2015-05-28 14:22:04 -0700

    Just pushing so the current version is live`

--- examples/pullSeriesData.py
+++ examples/pullSeriesData.py
@@ -12,14 +12,57 @@ MONGODB_URI = 'mongodb://rbarnes:tgb47atgb47a@ds047040.mongolab.com:47040/magres
 conn = pymongo.MongoClient(MONGODB_URI) # Connect to the database that I purchased
 db = conn.magresdata 
 collection = db.hanLabODNPTest # This is my test collection 
-searchDict = {'setType':'seriesData','macroMolecule':'CheY','bindingPartner':'None'}
+searchDict = {'setType':'seriesData','macroMolecule':'CheY','bindingPartner':'None','osmolyte':'None'}
 
 listOfSets = list(collection.find(searchDict))
 dataTag = 'tau'
 fig = figure(figsize=(15,8))
 for count,dataSet in enumerate(listOfSets):
     tau = dtb.dictToNdData(dataTag,dataSet) # setting retValue to True gives me the fit value instead of the power data. Note this only works for kSigma and nothing else for now.
-    plot(tau*1e12,'.',markersize=10,label='Urea %s'%tau.other_info.get('osmolyteConcentration'))
+    tau.sort('site')
+    plot(tau*1e12,'--.',markersize=20,label='native state')
+
+searchDict = {'setType':'seriesData','macroMolecule':'CheY','bindingPartner':'None','osmolyte':'urea'}
+
+listOfSets = list(collection.find(searchDict))
+dataTag = 'tau'
+for count,dataSet in enumerate(listOfSets):
+    tau = dtb.dictToNdData(dataTag,dataSet) # setting retValue to True gives me the fit value instead of the power data. Note this only works for kSigma and nothing else for now.
+    tau.sort('site')
+    plot(tau*1e12,'--.',markersize=20,label='urea denatured')
+
+searchDict = {'setType':'seriesData','macroMolecule':'CheYPep','bindingPartner':'None','osmolyte':'None'}
+
+listOfSets = list(collection.find(searchDict))
+dataTag = 'tau'
+for count,dataSet in enumerate(listOfSets):
+    tau = dtb.dictToNdData(dataTag,dataSet) # setting retValue to True gives me the fit value instead of the power data. Note this only works for kSigma and nothing else for now.
+    tau.sort('site')
+    plot(tau*1e12,'--.',markersize=20,label='peptide')
+
+searchDict = {'setType':'seriesData','macroMolecule':'CheY','bindingPartner':'P2'}
+
+listOfSets = list(collection.find(searchDict))
+dataTag = 'tau'
+tau = dtb.dictToNdData(dataTag,listOfSets[0]) # setting retValue to True gives me the fit value instead of the power data. Note this only works for kSigma and nothing else for now.
+siteList = [17.,37.,41.,62.,91.,121.]
+tauList = []
+tauError = []
+for count, value in enumerate(siteList):
+    print "looping over ",value
+    tauListTemp = []
+    tauErrorTemp = []
+    for count1,value1 in enumerate(tau.getaxis('site')):
+        if value1 == value:
+            print "Averaging site ",value1
+            tauListTemp.append(tau['site',count1].runcopy(real).data)
+            tauErrorTemp.append(tau['site',count1].get_error())
+    tauList.append(average(array(tauListTemp)))
+    tauError.append(average(array(tauErrorTemp)))
+tau = nddata(array(tauList)).rename('value','site').labels('site',array(siteList)).set_error(array(tauError))
+
+tau.sort('site')
+plot(tau*1e12,'--.',markersize=20,label='P2 bound')
 
 legend()
 legend(loc=4,prop={'size':25})
@@ -29,7 +72,7 @@ yticks(fontsize=20)
 ylabel(r'$\mathtt{\tau_c\/ [ps]}$',fontsize=30) 
 title(r'$\mathtt{CheY\/ Hydration\/ Dynamics}$',fontsize=30)
 fig.patch.set_alpha(0) # This makes the background transparent!!
-giveSpace()
+giveSpace(spaceVal = 0.01)
 tight_layout()
 
 
